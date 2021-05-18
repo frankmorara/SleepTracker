c9036218281805a76e3e0d42f2b6278def3f4cd2
frankmorara <franklinenyabuto11@gmail.com>
2021-05-18 09:28:35 +0300

    multithreading using coroutines

--- app/src/main/java/com/example/android/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/android/guesstheword/MainActivity.kt
@@ -14,4 +14,19 @@
  * limitations under the License.
  */
 
-include ':app'
+package com.example.android.guesstheword
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Creates an Activity that hosts all of the fragments in the app
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
